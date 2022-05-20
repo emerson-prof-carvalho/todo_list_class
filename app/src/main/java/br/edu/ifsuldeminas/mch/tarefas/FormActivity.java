66e2413ca28bb21b0a9fb49b56ccd0e46b62fead
Emerson Carvalho <emerson@MacBook-Pro-de-Emerson.local>
2022-05-20 20:08:18 -0300

    Layout do app

---
+++ app/src/main/java/br/edu/ifsuldeminas/mch/tarefas/FormActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsuldeminas.mch.tarefas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class FormActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
